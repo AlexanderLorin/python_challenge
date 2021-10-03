7f8d0e8cd98e97298a6a1496e8e8d4648aecfae2
lorinalexd@gmail.com <lorinalexd@gmail.com>
2021-10-02 23:46:24 -0400

    upload homework with codes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,25 +1,43 @@
 import csv
 
-with open("Resources/budget_data.csv","r") as csvfile:
+with open("Python_Challenge/PyBank/Resources/budget_data.csv","r") as csvfile:
     csvreader = csv.reader(csvfile)
     number_of_months = 0
     total_dollars = 0 
+    max_change_value = 0
+    min_change_value = 0
     #skip first row because it contains column names
     columnnames = next(csvreader)
+   
     
     for row in csvreader:
-        number_of_months +=1
-        total_dollars += int(row[1])
 
-with open("Analysis/budget_analysis.txt","w") as budget_analysis:
+        #count number of months
+        number_of_months +=1
+        #sum profit/losses
+        total_dollars += int(row[1])
+        #find average of profit/loss
+        average_change = total_dollars/number_of_months
+        #find max change value and the month
+        if max_change_value < int(row[1]):
+            max_change_value = int(row[1])
+            max_change_month = (row[0])
+        #find min change value and the month
+        if min_change_value > int(row[1]):
+            min_change_value = int(row[1])
+            min_change_month = (row[0])            
+
+
+            
+with open("Python_Challenge/PyBank/Analysis/budget_analysis.txt","w") as budget_analysis:
     summary_report = f"""
 Financial Analysis
 ----------------------------
 Total Months: {number_of_months}
 Total: ${total_dollars}
-Average  Change: $-2315.12
-Greatest Increase in Profits: Feb-2012 ($1926159)
-Greatest Decrease in Profits: Sep-2013 ($-2196167)
+Average  Change: ${average_change}
+Greatest Increase in Profits: {max_change_month} {max_change_value}
+Greatest Decrease in Profits: {min_change_month} {min_change_value}
     """
     print(summary_report)
     budget_analysis.write(summary_report)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,41 @@
 import csv
 
-with open("Resources/election_data.csv","r") as csvfile:
+with open("Python_Challenge/PyPoll/Resources/election_data.csv","r") as csvfile:
     csvreader = csv.reader(csvfile)
- 
+
+    
     #skip first row because it contains column names
     columnnames = next(csvreader)
+    total_votes = len(list(csvreader))
     
-    for row in csvreader:
-        pass
 
-with open("Analysis/election_analysis.txt","w") as election_analysis:
+    candidate1 = "Khan"
+    candidate2 = "Correy"
+    candidate3 = "Li"
+    candidate4 = "O'Tooley"
+
+    candidate1_vote = 0
+    candidate2_vote = 0 
+    candidate3_vote = 0
+    candidate4_vote = 0
+
+
+    for row in csvreader:
+        if str(row[2]) == candidate1:
+            candidate1_vote +=1
+        
+
+
+with open("Python_Challenge/PyPoll/Analysis/election_analysis.txt","w") as election_analysis:
     summary_report = f"""
 Election Results
 -------------------------
-Total Votes: 3521001
+Total Votes: {total_votes}
 -------------------------
-Khan: 63.000% (2218231)
-Correy: 20.000% (704200)
-Li: 14.000% (492940)
-O'Tooley: 3.000% (105630)
+{candidate1}: 63.000% (2218231)
+{candidate1}: 20.000% (704200)
+{candidate1}: 14.000% (492940)
+{candidate1}: 3.000% (105630)
 -------------------------
 Winner: Khan
 -------------------------
